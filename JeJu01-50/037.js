09d3b01d293570d5403510c1e3b45cb979bf64b8
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-10-25 16:13:09 +0900

    반장선거
    뽑은 후보들 입력받아 뽑힌 학생의 이름과 표수를 출력하는 프로그램.

--- JeJu01-50/037.js
+++ JeJu01-50/037.js
@@ -2,12 +2,16 @@
 // const names = prompt('').split(' ');
   const array = ["원범", "원범", "혜원","혜원","혜원","혜원","유진","유진"]
 
+  // for문 대신 reduce 사용하기
   const result = array.reduce((result, name) => {
     result[name] = result[name] === undefined ? 1 : result[name] + 1;
     return  result;
   },{});
 
+  // key 배열로 sort하기  
   const winner = Object.keys(result).sort((a,b) => 
-    result[a] - result[b]
+    result[b] - result[a]
 )[0];
+
+console.log(`${winner}(이)가 총 ${result[winner]}표로 반장이 되었습니다.`);
 
