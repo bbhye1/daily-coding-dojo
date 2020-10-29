766e94bb79f21ab7ca4eb4c3ff8c2e1171581a0e
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-10-29 22:37:31 +0900

    set 자료형의 응용
    주어진 데이터의 중복된 데이터를 삭제해서 개수 출력하기

---
+++ JeJu01-50/047.js
@@ -0,0 +1,26 @@
+  // 주어진것 
+  const people = {
+    이호준: "01050442903",
+    이호상: "01051442904",
+    이준호: "01050342904",
+    이호준: "01050442903",
+    이준: "01050412904",
+    이호: "01050443904",
+    이호준: "01050442903"
+  };
+
+  // 풀이
+  const keys = Object.keys(people);
+  console.log(keys.length);
+
+
+  // 문제의 답 풀이 
+
+  let result = new Set();
+
+  for (let key in people) {
+    result.add(people[key]);
+  }
+  console.log(result.size);
+
+  
