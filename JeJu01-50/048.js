ad779b06e344bba282f7b57bcef436448b1072b1
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-11-05 23:03:28 +0900

    제일 작은 수 제거하기

--- JeJu01-50/048.js
+++ JeJu01-50/048.js
@@ -3,8 +3,19 @@ const input = 'AAABBBcccddd';
 const result = input
     .split('')
     .map((item) => { 
-      item === item.toUpperCase() ? item.toLowerCase() : item.toUpperCase();
+     return item === item.toUpperCase() ? item.toLowerCase() : item.toUpperCase();
     })
     .join('');
 
 console.log(result);
+
+
+// 원래답안
+for (let i=0; i<a.length; i++){
+	//toLowerCase() 메서드는 문자열을 소문자로, toUpperCase() 메서드는 문자열을 대문자로 변환하여 반환합니다.
+  if(a[i] === a[i].toLowerCase()){ 
+    b.push(a[i].toUpperCase());
+  } else {
+    b.push(a[i].toLowerCase());
+  }
+}
